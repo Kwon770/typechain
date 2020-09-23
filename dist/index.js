b33cb7403a255b3a254ea5d2beffbcf5b8d6c3bf
Kwon770 <sckwon770@gmail.com>
2020-09-24 00:02:03 +0900

    0.7 Classes on Typescript part One

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const me = {
-    name: "kwon",
-    age: 22,
-    gender: "male",
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const nut = new Human("black", 25, "male");
+// const me = {
+//   name: "kwon",
+//   age: 22,
+//   gender: "male",
+// };
 const sayHi = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
-console.log(sayHi(me));
+console.log(sayHi(nut));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,27 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const me = {
-  name: "kwon",
-  age: 22,
-  gender: "male",
-};
+const nut = new Human("black", 25, "male");
+
+// const me = {
+//   name: "kwon",
+//   age: 22,
+//   gender: "male",
+// };
 
 const sayHi = (person: Human): string => {
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(me));
+console.log(sayHi(nut));
 
 export {};
