8891b8ed25e70c112f14e30856166a94f9fc7d64
Kwon770 <sckwon770@gmail.com>
2020-09-23 23:46:06 +0900

    0.5 Types in Typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+console.log(sayHi("sck", 444, "male"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+  return `Hello ${name}, you are ${age}, you are a ${gender}`;
+};
+
+console.log(sayHi("sck", 444, "male"));
+
+export {};
