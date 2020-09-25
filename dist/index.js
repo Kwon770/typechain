64535308e71762ac23e1701db4fbacda47c7f057
Kwon770 <sckwon770@gmail.com>
2020-09-25 09:58:19 +0900

    feat: 0.10  Creating a Block part three

--- dist/index.js
+++ dist/index.js
@@ -16,4 +16,13 @@ let blockchain = [genesisBlock];
 const getBlockchain = () => blockchain;
 const getLatestBlock = () => blockchain[blockchain.length - 1];
 const getNewTimestamp = () => Math.round(new Date().getTime() / 1000);
+const createNewBlock = (data) => {
+    const previousBlock = getLatestBlock();
+    const newIndex = previousBlock.index + 1;
+    const newTimestamp = getNewTimestamp();
+    const newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    return newBlock;
+};
+console.log(createNewBlock("hello"), createNewBlock("bye"));
 //# sourceMappingURL=index.js.map
